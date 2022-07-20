1c50717aaa1a906b4407b7bbc5888cbd73f7499b
Marcos Gabriel Quenup Carvalho <101126311+Quenupp@users.noreply.github.com>
2022-07-20 20:03:07 -0300

    Add files via upload

---
+++ MG Proj 3/src/resizeImage.java
@@ -0,0 +1,16 @@
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+
+public class resizeImage {
+
+    BufferedImage resizeImage(BufferedImage originalImage, int targetWidth, int targetHeight) throws IOException {
+        BufferedImage resizedImage = new BufferedImage(targetWidth, targetHeight, BufferedImage.TYPE_INT_RGB);
+        Graphics2D graphics2D = resizedImage.createGraphics();
+        graphics2D.drawImage(originalImage, 0, 0, targetWidth, targetHeight, null);
+        graphics2D.dispose();
+        return resizedImage;
+    }
+
+}
